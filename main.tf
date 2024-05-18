53608ab148e41a338b32cbd00ee7ad3271bdfb4d
Egoorbis <egoorbis@users.noreply.github.com>
2024-05-18 11:53:19 +0200

    chore: Update terraform version requirement and provider configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 1.6.0"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
   location = var.location
